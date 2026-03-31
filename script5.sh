7f6c6023700603ca52d244d190aed641348f3244
Khyati Mourya <khyati1mourya@gmail.com>
2026-03-31 19:12:15 +0530

    Update script5.sh

--- script5.sh
+++ script5.sh
@@ -6,6 +6,7 @@ read -p "1. Name one open-source tool you use every day: " TOOL
 read -p "2. In one word, what does 'freedom' mean to you? " FREEDOM
 read -p "3. Name one thing you would build and share freely: " BUILD
 DATE=$(date '+%d %B %Y')
+
 OUTPUT="manifesto_$(whoami).txt"
 echo "--- Open Source Manifesto ---" > "$OUTPUT"
 echo "Generated on: $DATE" >> "$OUTPUT"
